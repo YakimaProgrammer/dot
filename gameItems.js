e958767647b237dcd106fe89d2749f484bb1052a
Magnus Fulton <magnusjackfulton@gmail.com>
2020-11-14 20:21:01 -0800

    Instead of using images, all shapes are now represented in 3D!

---
+++ gameItems.js
@@ -0,0 +1,283 @@
+// This is where stuff in our game will happen:
+var scene = new THREE.Scene();
+
+// This is what sees the stuff:
+var aspect_ratio = window.innerWidth / window.innerHeight;
+var camera = new THREE.PerspectiveCamera(75, aspect_ratio, 1, 10000);
+camera.position.z = 500;
+scene.add(camera);
+
+// This will draw what the camera sees onto the screen:
+var renderer = new THREE.CanvasRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+// ******** START CODING ON THE NEXT LINE ********
+const SIZE = 25;
+const colors = {
+  RED : 0xe60b0b,
+  GRAY : 0x695c5c,
+  ORANGE : 0xf08e0e,
+  YELLOW : 0xffff21,
+  GREEN : 0x30c702,
+  BLUE : 0x0335ff,
+  BLACK : 0x000000
+};
+
+var settings = {
+  CUBESCALEDOWN : 2
+  
+}
+
+
+function newMagnet() {
+  var shape, cover, origin, sidePillar, sign;
+  var size = SIZE / 1.25;
+  origin = new THREE.Object3D();
+  //origin.position.set(0,-size*2,0);
+
+  shape = new THREE.CubeGeometry(size*2,size,size);
+  cover = new THREE.MeshBasicMaterial({color: colors.RED});
+  sidePillar = new THREE.Mesh(shape, cover);
+  sidePillar.position.set(0,-size*2,0);
+  origin.add(sidePillar);
+  
+  for (sign of [-1, 1]) {
+    shape = new THREE.CubeGeometry(size,size*2,size);
+    cover = new THREE.MeshBasicMaterial({color: colors.RED});
+    sidePillar = new THREE.Mesh(shape, cover);
+    sidePillar.position.set(size*1.5*sign,-size*0.5,0);
+    origin.add(sidePillar);
+    
+    shape = new THREE.CubeGeometry(size,size,size);
+    cover = new THREE.MeshBasicMaterial({color: colors.GRAY});
+    sidePillar = new THREE.Mesh(shape, cover);
+    sidePillar.position.set(size*1.5*sign,size,0);
+    origin.add(sidePillar);
+  }
+  
+  return origin;
+}
+
+function newCoin(size = SIZE/2) {
+  var shape, cover, origin, sidePillar, sign;
+  
+  origin = new THREE.Object3D();
+  
+  shape = new THREE.CubeGeometry(size,size*3,size);
+  cover = new THREE.MeshBasicMaterial({color: colors.ORANGE});
+  sidePillar = new THREE.Mesh(shape, cover);
+  origin.add(sidePillar);
+  
+  for (sign of [-1,1]) {
+    shape = new THREE.CubeGeometry(size*2,size*3,size);
+    cover = new THREE.MeshBasicMaterial({color: colors.YELLOW});
+    sidePillar = new THREE.Mesh(shape, cover);
+    sidePillar.position.set(size*1.5*sign,0,0);
+    origin.add(sidePillar);
+    
+    shape = new THREE.CubeGeometry(size*3,size,size);
+    cover = new THREE.MeshBasicMaterial({color: colors.YELLOW});
+    sidePillar = new THREE.Mesh(shape, cover);
+    sidePillar.position.set(0,size*2*sign,0);
+    origin.add(sidePillar);
+  }
+  
+  return origin;
+}
+
+function newCoinMagnet() {
+  var origin = newMagnet();
+  origin.add(newCoin(SIZE/3));
+  return origin;
+}
+
+function newSpeedBoost() {
+  var shape, cover, origin, sidePillar, leadingCube, trailingCube, i, x;
+  var currentColor = [colors.RED,colors.YELLOW,colors.GREEN];
+  var size = SIZE / 3.5; //this thing is HUGE! Let's scale it down a bit!
+  origin = new THREE.Object3D();
+  
+  for (i = 0; i < 6; i++) {
+    shape = new THREE.CubeGeometry(size,size,size);
+    cover = new THREE.MeshBasicMaterial({color: currentColor[Math.abs(i % 3)]});
+    leadingCube = new THREE.Mesh(shape, cover);
+    leadingCube.position.set(size*i,0,0);
+    
+    for (x = 0; x < 6; x++) {
+      shape = new THREE.CubeGeometry(size,size,size);
+      cover = new THREE.MeshBasicMaterial({color: currentColor[Math.abs(i % 3)]});
+      trailingCube = new THREE.Mesh(shape, cover);
+      trailingCube.position.set(-size*x,size*x,0);
+      leadingCube.add(trailingCube);
+      
+      shape = new THREE.CubeGeometry(size,size,size);
+      cover = new THREE.MeshBasicMaterial({color: currentColor[Math.abs(i % 3)]});
+      trailingCube = new THREE.Mesh(shape, cover);
+      trailingCube.position.set(-size*x,-size*x,0);
+      leadingCube.add(trailingCube);
+    }
+    
+    origin.add(leadingCube);
+  }    
+  
+  return origin;
+}
+
+function newLevelUp() {
+  var shape, cover, origin, sidePillar, i, plus, sign;
+  var size = SIZE / 3;
+  origin = new THREE.Object3D();
+  
+  shape = new THREE.CubeGeometry(size,size*10,size);
+  cover = new THREE.MeshBasicMaterial({color: colors.GREEN});
+  sidePillar = new THREE.Mesh(shape, cover);
+  origin.add(sidePillar);
+  
+  for (i = 0; i < 5; i++) {
+    shape = new THREE.CubeGeometry(size*(9-2*i),size,size);
+    cover = new THREE.MeshBasicMaterial({color: colors.GREEN});
+    sidePillar = new THREE.Mesh(shape, cover);
+    sidePillar.position.set(0,size*i,0);
+    origin.add(sidePillar);
+  }
+  
+  for (sign of [-1,1]) {
+    shape = new THREE.CubeGeometry(size,size*3,size);
+    cover = new THREE.MeshBasicMaterial({color: colors.YELLOW});
+    plus = new THREE.Mesh(shape, cover);
+    
+    shape = new THREE.CubeGeometry(size*3,size,size);
+    cover = new THREE.MeshBasicMaterial({color: colors.YELLOW});
+    plus.add(new THREE.Mesh(shape, cover));
+  
+    plus.position.set(size*3*sign,size*-3,0);
+    origin.add(plus);
+  }    
+  
+  return origin;
+}
+
+function newCube() {
+  var shape, cover, origin, sidePillar, signX, signY, signZ;
+  var size = SIZE / settings.CUBESCALEDOWN;
+  origin = new THREE.Object3D();
+  
+  for (signZ of [-1, 1]) {
+    for (signY of [-1,1]) {
+      shape = new THREE.CubeGeometry(size*3,size,size);
+      cover = new THREE.MeshBasicMaterial({color: colors.BLUE});
+      sidePillar = new THREE.Mesh(shape, cover);
+      sidePillar.position.set(0,size*2*signY,size*2*signZ);
+      origin.add(sidePillar);
+    }
+    
+    for (signX of [-1,1]) {
+      shape = new THREE.CubeGeometry(size,size*3,size);
+      cover = new THREE.MeshBasicMaterial({color: colors.BLUE});
+      sidePillar = new THREE.Mesh(shape, cover);
+      sidePillar.position.set(size*2*signX,0,size*2*signZ);
+      origin.add(sidePillar);
+    }
+  }
+  
+  for (signX of [-1,1]) {
+    for (signY of [-1,1]) {
+      shape = new THREE.CubeGeometry(size,size,size*3);
+      cover = new THREE.MeshBasicMaterial({color: colors.BLUE});
+      sidePillar = new THREE.Mesh(shape, cover);
+      sidePillar.position.set(size*2*signY,size*2*signX,0);
+      origin.add(sidePillar);
+    }
+  }
+  
+  return origin;
+}
+
+
+function newPhase() {
+  var origin, shape, cover, cube, signZ;
+  var size = SIZE / settings.CUBESCALEDOWN;
+  origin = newCube();
+  shape = new THREE.CubeGeometry(size*3-1,size*3-1,size);
+  cover = new THREE.MeshBasicMaterial({color: colors.BLACK});
+  origin.add(new THREE.Mesh(shape, cover));
+  
+  for (signZ of [-1,1]) {
+    shape = new THREE.CubeGeometry(size/2,size/2,size/2);
+    cover = new THREE.MeshBasicMaterial({color: colors.GREEN});
+    cube = new THREE.Mesh(shape, cover);
+    cube.position.set(0,0,size*signZ);
+    origin.add(cube);
+  } 
+  return origin;
+}
+
+function randomPositiveOrNegative() {
+  return Math.random() * (!!Math.round(Math.random()) ? 1 : -1);
+}
+
+function newScrabbler() {
+  var origin, shape, cover, cube, i;
+  var size = SIZE / settings.CUBESCALEDOWN;
+  origin = newCube();
+  
+  for (i = 0; i < 10; i++) {
+    shape = new THREE.CubeGeometry(size/2,size/2,size/2);
+    cover = new THREE.MeshBasicMaterial({color: colors.RED});
+    cube = new THREE.Mesh(shape, cover);
+    cube.position.set(
+      size * 1.25 * randomPositiveOrNegative(),
+      size * 1.25 * randomPositiveOrNegative(),
+      size * 1.25 * randomPositiveOrNegative()
+    );
+    
+    cube.rotation.set(
+      Math.PI * randomPositiveOrNegative() * 2,
+      Math.PI * randomPositiveOrNegative() * 2,
+      Math.PI * randomPositiveOrNegative() * 2
+    );
+    origin.add(cube);
+  } 
+  return origin;
+}
+
+function newHeart() {
+  var origin, shape, cover, sidePillar, signX;
+  var size = SIZE / 3;
+  origin = new THREE.Object3D();
+  
+  shape = new THREE.CubeGeometry(size,size*7,size);
+  cover = new THREE.MeshBasicMaterial({color: colors.RED});
+  sidePillar = new THREE.Mesh(shape, cover);
+  sidePillar.position.set(0,-size*1.5,0);
+  origin.add(sidePillar);
+  
+  for (signX of [-1,1]) {
+    shape = new THREE.CubeGeometry(size,size*8,size);
+    cover = new THREE.MeshBasicMaterial({color: colors.RED});
+    sidePillar = new THREE.Mesh(shape, cover);
+    sidePillar.position.set(size*signX,0,0);
+    origin.add(sidePillar);
+    
+    shape = new THREE.CubeGeometry(size,size*7,size);
+    cover = new THREE.MeshBasicMaterial({color: colors.RED});
+    sidePillar = new THREE.Mesh(shape, cover);
+    sidePillar.position.set(size*2*signX,size*.5,0);
+    origin.add(sidePillar);
+    
+    shape = new THREE.CubeGeometry(size,size*6,size);
+    cover = new THREE.MeshBasicMaterial({color: colors.RED});
+    sidePillar = new THREE.Mesh(shape, cover);
+    sidePillar.position.set(size*3*signX,size,0);
+    origin.add(sidePillar);
+    
+    shape = new THREE.CubeGeometry(size,size*4,size);
+    cover = new THREE.MeshBasicMaterial({color: colors.RED});
+    sidePillar = new THREE.Mesh(shape, cover);
+    sidePillar.position.set(size*4*signX,size,0);
+    origin.add(sidePillar);
+  }    
+  
+  return origin;
+}
--- objects.js
+++ objects.js
@@ -8,3 +8,128 @@ grid = new PF.Grid(3,3, [
 var finder = new PF.AStarFinder();
 var path = finder.findPath(0, 0, 2, 2, grid);
 */
+
+
+//Define a whole bunch of tile states so I don't get confused by magic constants
+const tileStates = {
+	EMPTY : 0,
+	WALL : 1,
+	COIN : 2,
+	MAGNET : 3,
+	HEART : 4,
+	LEVELUP : 5,
+	PHAZER : 6,
+	SCRABBLER : 7
+}
+
+const settings = {
+	
+	INCREASEBY : 1.3,
+	
+	MINLEVEL : {
+		[tileStates.WALL] : 0,
+		[tileStates.COIN] : 0,
+		[tileStates.HEART] : 3,
+		[tileStates.MAGNET] : 5,
+		[tileStates.SCRABBLER] : 7,
+		[tileStates.PHAZER] : 9,
+	}
+	
+	DENSITY : {
+		[tileStates.WALL] : 0.15,
+		[tileStates.COIN] : 0.1,
+		[tileStates.HEART] : 0.1, 
+	}
+	
+    CUBESCALEDOWN : 2,
+}
+
+var currentLevel = {
+	MAP : null,
+	PLAYER : null,
+	HUNTER : null,
+	level : 0
+}
+
+Object.freeze(tileStates);
+Object.freeze(settings);
+
+function twoDarray(x, y, fill) {
+	var dx = new Array(x).fill(fill);
+	for (var i = 0; i < y; i++) {
+		dx[i] = new Array(y).fill(fill);
+	}
+	return dx;
+}
+
+class tileObject {
+	constructor(type, posX, posY) {
+		this.type = type;
+		this.posX = posX;
+		this.posY = posY;
+	}
+	
+	toWallState() {
+		return this.type == tileStates.WALL ? 1 : 0;
+	}
+}
+
+class map {
+	constructor(widthX, heightY) {
+		currentLevel.MAP = this;
+		
+		this.widthX = widthX;
+		this.heightY = heightY;
+		
+		this.tiles = twoDarray(widthX, heightY, tileStates.EMPTY);
+		
+		this.numTiles = widthX * heightY;
+		
+		//First, generate all game items
+		
+		for (var gameItem of settings.DENSITY) {
+			if (settings.MINLEVEL[gameItem] >= currentLevel.level) {
+				for (var i = 0; i++; i < this.getItemDensity(gameItem)) {
+					var x, y = this.getRandomAvailablePoint();
+					this.tiles[x][y] = gameItem;
+				}
+			}
+		}
+		
+		var playerX, playerY, hunterX, hunterY;
+		var stillSpawning = true;
+		
+		while (stillSpawning) {
+			playerX, playerY = this.getRandomAvailablePoint();
+			hunterX, hunterY = this.getRandomAvailablePoint();
+			
+			if (((playerX - hunterX) ** 2 + (playerY - hunterY) ** 2)) < 5) {
+				continue; //force next loop now
+			}
+			
+			
+			
+		}
+		
+		//Now, check that I can access the level up, and that the HUNTER can access me
+		
+		
+	}
+	
+	getItemDensity(key) {
+		return Math.floor((settings.DENSITY[key] * (settings.INCREASEBY ** currentLevel.level)) * this.numTiles)
+	}
+	
+	getRandomAvailablePoint() {
+		while (true) {
+			var x = Math.floor(this.widthX * Math.random());
+			var y = Math.floor(this.heightY * Math.random());
+			
+			if (this.tiles[x][y] == tileStates.EMPTY) {
+				return [x,y]
+			}
+		}
+	}
+}
+
+currentLevel.MAP = new map(50,60)
